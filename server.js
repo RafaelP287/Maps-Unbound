840e0ba48d0d03cd5ef1db2a228d97a1cacd726c
Rian-stack <lpapa@students.avc.edu>
2025-12-13 13:42:10 -0800

    implemented login and authentication. TODO: link to database

--- server.js
+++ server.js
@@ -1,27 +1,45 @@
-require('dotenv').config(); // Load env variables
 const express = require('express');
-const mongoose = require('mongoose');
+const app = express(); 
+const bcrypt = require('bcrypt'); // allows password hashing for security
 
-const app = express();
+app.use(express.json()); //allows application to accept JSON
 
-// Middleware
-app.use(express.json());
+const users = []; 
 
-// Database Connection
-const connectDB = async () => {
-  try {
-    await mongoose.connect(process.env.MONGO_URI);
-    console.log('✅ MongoDB Connected Successfully!');
-  } catch (error) {
-    console.error('❌ MongoDB Connection Failed:', error.message);
-    process.exit(1);
-  }
-};
+// Endpoint to get all users
+app.get('/users' , (req, res) => {
+    res.json(users);
+}); 
 
-connectDB();
+// Endpoint to create a new user
+app.post('/users', async (req, res) => {
+    try {
+      const salt = await bcrypt.genSalt();
+      const hashedPassword = await bcrypt.hash(req.body.password, 10); 
+      // console.log(salt);
+      // console.log(hashedPassword);
+      const user = { name: req.body.name, password: hashedPassword };
+      users.push(user);
+      res.status(201).send();
+    } catch {
+      res.status(500).send();
+    }
+})
 
-const PORT = process.env.PORT || 5000;
-
-app.listen(PORT, () => {
-  console.log(`🚀 Server running on port ${PORT}`);
-});
+// Endpoint to login a user
+app.post('/users/login', async (req, res) => {
+    const user = users.find(user => user.name === req.body.name); 
+    if (user == null) {
+        return res.status(400).send('Cannot find user');
+    }
+    try {
+        if(await bcrypt.compare(req.body.password, user.password)) {
+            res.send('Success');
+        } else {
+            res.send('Not Allowed');
+        }
+    } catch {
+        res.status(500).send();
+    }
+})
+app.listen(3000);
