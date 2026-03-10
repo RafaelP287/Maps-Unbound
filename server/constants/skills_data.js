ac3fa0be50321e8c116ac3aa75fd29cf29eb0635
tensofu <148308017+TerriblyFriedRice@users.noreply.github.com>
2026-03-10 05:02:42 -0700

    formatting skills data

--- server/constants/skills_data.js
+++ server/constants/skills_data.js
@@ -1,20 +1,182 @@
 export const DEFAULT_SKILLS = [
-  { api_index: "acrobatics", name: "Acrobatics", desc: [], ability_score: { index: "dex", name: "DEX", url: "/api/2014/ability-scores/dex" } },
-  { api_index: "animal-handling", name: "Animal Handling", desc: [], ability_score: { index: "wis", name: "WIS", url: "/api/2014/ability-scores/wis" } },
-  { api_index: "arcana", name: "Arcana", desc: [], ability_score: { index: "int", name: "INT", url: "/api/2014/ability-scores/int" } },
-  { api_index: "athletics", name: "Athletics", desc: [], ability_score: { index: "str", name: "STR", url: "/api/2014/ability-scores/str" } },
-  { api_index: "deception", name: "Deception", desc: [], ability_score: { index: "cha", name: "CHA", url: "/api/2014/ability-scores/cha" } },
-  { api_index: "history", name: "History", desc: [], ability_score: { index: "int", name: "INT", url: "/api/2014/ability-scores/int" } },
-  { api_index: "insight", name: "Insight", desc: [], ability_score: { index: "wis", name: "WIS", url: "/api/2014/ability-scores/wis" } },
-  { api_index: "intimidation", name: "Intimidation", desc: [], ability_score: { index: "cha", name: "CHA", url: "/api/2014/ability-scores/cha" } },
-  { api_index: "investigation", name: "Investigation", desc: [], ability_score: { index: "int", name: "INT", url: "/api/2014/ability-scores/int" } },
-  { api_index: "medicine", name: "Medicine", desc: [], ability_score: { index: "wis", name: "WIS", url: "/api/2014/ability-scores/wis" } },
-  { api_index: "nature", name: "Nature", desc: [], ability_score: { index: "int", name: "INT", url: "/api/2014/ability-scores/int" } },
-  { api_index: "perception", name: "Perception", desc: [], ability_score: { index: "wis", name: "WIS", url: "/api/2014/ability-scores/wis" } },
-  { api_index: "performance", name: "Performance", desc: [], ability_score: { index: "cha", name: "CHA", url: "/api/2014/ability-scores/cha" } },
-  { api_index: "persuasion", name: "Persuasion", desc: [], ability_score: { index: "cha", name: "CHA", url: "/api/2014/ability-scores/cha" } },
-  { api_index: "religion", name: "Religion", desc: [], ability_score: { index: "int", name: "INT", url: "/api/2014/ability-scores/int" } },
-  { api_index: "sleight-of-hand", name: "Sleight of Hand", desc: [], ability_score: { index: "dex", name: "DEX", url: "/api/2014/ability-scores/dex" } },
-  { api_index: "stealth", name: "Stealth", desc: [], ability_score: { index: "dex", name: "DEX", url: "/api/2014/ability-scores/dex" } },
-  { api_index: "survival", name: "Survival", desc: [], ability_score: { index: "wis", name: "WIS", url: "/api/2014/ability-scores/wis" } }
+  {
+    api_index: "acrobatics",
+    name: "Acrobatics",
+    desc: [],
+    ability_score: {
+      index: "dex",
+      name: "DEX",
+      url: "/api/2014/ability-scores/dex",
+    },
+  },
+  {
+    api_index: "animal-handling",
+    name: "Animal Handling",
+    desc: [],
+    ability_score: {
+      index: "wis",
+      name: "WIS",
+      url: "/api/2014/ability-scores/wis",
+    },
+  },
+  {
+    api_index: "arcana",
+    name: "Arcana",
+    desc: [],
+    ability_score: {
+      index: "int",
+      name: "INT",
+      url: "/api/2014/ability-scores/int",
+    },
+  },
+  {
+    api_index: "athletics",
+    name: "Athletics",
+    desc: [],
+    ability_score: {
+      index: "str",
+      name: "STR",
+      url: "/api/2014/ability-scores/str",
+    },
+  },
+  {
+    api_index: "deception",
+    name: "Deception",
+    desc: [],
+    ability_score: {
+      index: "cha",
+      name: "CHA",
+      url: "/api/2014/ability-scores/cha",
+    },
+  },
+  {
+    api_index: "history",
+    name: "History",
+    desc: [],
+    ability_score: {
+      index: "int",
+      name: "INT",
+      url: "/api/2014/ability-scores/int",
+    },
+  },
+  {
+    api_index: "insight",
+    name: "Insight",
+    desc: [],
+    ability_score: {
+      index: "wis",
+      name: "WIS",
+      url: "/api/2014/ability-scores/wis",
+    },
+  },
+  {
+    api_index: "intimidation",
+    name: "Intimidation",
+    desc: [],
+    ability_score: {
+      index: "cha",
+      name: "CHA",
+      url: "/api/2014/ability-scores/cha",
+    },
+  },
+  {
+    api_index: "investigation",
+    name: "Investigation",
+    desc: [],
+    ability_score: {
+      index: "int",
+      name: "INT",
+      url: "/api/2014/ability-scores/int",
+    },
+  },
+  {
+    api_index: "medicine",
+    name: "Medicine",
+    desc: [],
+    ability_score: {
+      index: "wis",
+      name: "WIS",
+      url: "/api/2014/ability-scores/wis",
+    },
+  },
+  {
+    api_index: "nature",
+    name: "Nature",
+    desc: [],
+    ability_score: {
+      index: "int",
+      name: "INT",
+      url: "/api/2014/ability-scores/int",
+    },
+  },
+  {
+    api_index: "perception",
+    name: "Perception",
+    desc: [],
+    ability_score: {
+      index: "wis",
+      name: "WIS",
+      url: "/api/2014/ability-scores/wis",
+    },
+  },
+  {
+    api_index: "performance",
+    name: "Performance",
+    desc: [],
+    ability_score: {
+      index: "cha",
+      name: "CHA",
+      url: "/api/2014/ability-scores/cha",
+    },
+  },
+  {
+    api_index: "persuasion",
+    name: "Persuasion",
+    desc: [],
+    ability_score: {
+      index: "cha",
+      name: "CHA",
+      url: "/api/2014/ability-scores/cha",
+    },
+  },
+  {
+    api_index: "religion",
+    name: "Religion",
+    desc: [],
+    ability_score: {
+      index: "int",
+      name: "INT",
+      url: "/api/2014/ability-scores/int",
+    },
+  },
+  {
+    api_index: "sleight-of-hand",
+    name: "Sleight of Hand",
+    desc: [],
+    ability_score: {
+      index: "dex",
+      name: "DEX",
+      url: "/api/2014/ability-scores/dex",
+    },
+  },
+  {
+    api_index: "stealth",
+    name: "Stealth",
+    desc: [],
+    ability_score: {
+      index: "dex",
+      name: "DEX",
+      url: "/api/2014/ability-scores/dex",
+    },
+  },
+  {
+    api_index: "survival",
+    name: "Survival",
+    desc: [],
+    ability_score: {
+      index: "wis",
+      name: "WIS",
+      url: "/api/2014/ability-scores/wis",
+    },
+  },
 ];
